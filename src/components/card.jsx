ab5d98340794cab69f21f4849dd94f1862dd2300
Onnichan <dyangeltk17@gmail.com>
2022-05-02 00:16:39 -0500

    :lipstick: card component now has better design :)

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from './modal';
+import PokeballImage from '../assets/icons/pokeball.png';
 
 export default class Card extends React.Component{
 
@@ -8,6 +9,8 @@ export default class Card extends React.Component{
     this.state = {
       image: '',
       type: '',
+      order: '',
+      pokemon: {},
       showModal: false,
     }
     this.handleModal = this.handleModal.bind(this);
@@ -17,10 +20,12 @@ export default class Card extends React.Component{
     api.then(result => result.json())
     .then(data => {
       this.setState({
-        image: data.sprites['back_default'],
+        image: data.sprites['front_default'],
         type: data.types[0].type.name,
+        order: data.order,
+        pokemon: data,
       },()=>{
-        console.log(data);
+        // console.log('data', this.state.pokemon);
       });
     });
   }
@@ -38,11 +43,18 @@ export default class Card extends React.Component{
     return(
       <>
         <div className='card' onClick={this.handleModal} style={{ backgroundColor: `var(--bg-poke-color-light-${this.state.type})`}}>
-          <img className='card__image' src={this.state.image} alt="" />
+          <div className='card__title'>
+            <img className='card__title-img' src={PokeballImage} alt="" />
+            <span className='card__title-text'>{`#${this.state.order}`}</span>
+          </div>
+          <div className="card__badge">
+           {this.state.type}
+          </div>
           <h3 className='card__name'>{pokemon.name}</h3>
+          <img className='card__image' width={120} height={120} src={this.state.image} alt="" />
         </div>
         {
-          this.state.showModal && <Modal onHandleModal={this.handleModal} pokemon={pokemon}/>
+          this.state.showModal && <Modal onHandleModal={this.handleModal} pokemon={this.state.pokemon}/>
         }
       </>
     )
